febcb6a66c9dc2cceafd89f2855cae876e7d7624
Martin Herkt <lachs0r@srsfckn.biz>
2017-11-13 11:20:46 +0100

    sdl2: switch to hg tip

--- packages/sdl2.cmake
+++ packages/sdl2.cmake
@@ -1,8 +1,7 @@
 ExternalProject_Add(sdl2
     DEPENDS libiconv
-    URL "http://libsdl.org/release/SDL2-2.0.5.tar.gz"
-    URL_HASH SHA256=442038cf55965969f2ff06d976031813de643af9c9edc9e331bd761c242e8785
-    PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/sdl2-0001-remove-conflicting-xinput-structs.patch
+    HG_REPOSITORY "http://hg.libsdl.org/SDL"
+    UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
@@ -13,3 +12,6 @@ ExternalProject_Add(sdl2
     INSTALL_COMMAND ${MAKE} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
+
+force_rebuild_hg(sdl2)
+autogen(sdl2)
