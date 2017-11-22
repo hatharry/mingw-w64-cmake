17eea0bd48b51c8193c3e1e373e75afa646bd7a5
Martin Herkt <lachs0r@srsfckn.biz>
2017-11-22 16:00:12 +0100

    fribidi: switch to git master

--- packages/fribidi.cmake
+++ packages/fribidi.cmake
@@ -1,8 +1,15 @@
+if(NOT WIN32)
+    find_program(WINE wine)
+    set(TARGET_EXEC "TARGET_EXEC=${WINE}")
+endif()
+
 ExternalProject_Add(fribidi
     DEPENDS gcc
-    URL "http://fribidi.org/download/fribidi-0.19.7.tar.bz2"
-    URL_HASH SHA256=08222a6212bbc2276a2d55c3bf370109ae4a35b689acbc66571ad2a670595a8e
-    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
+    GIT_REPOSITORY "https://github.com/fribidi/fribidi.git"
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/fribidi-*.patch
+    # certain env variables with spaces confuse this broken build system
+    CONFIGURE_COMMAND ${EXEC} PKG_CONFIG=pkg-config <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
@@ -10,7 +17,11 @@ ExternalProject_Add(fribidi
         --without-glib
         --enable-charsets
         --enable-malloc
-    BUILD_COMMAND ${MAKE} -j1 # breaks with parallel make
+    BUILD_COMMAND ${MAKE} -j1 ${TARGET_EXEC} # breaks with parallel make
     INSTALL_COMMAND ${MAKE} -j1 install
+    BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
+
+force_rebuild_git(fribidi)
+autoreconf(fribidi)
