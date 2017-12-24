6077c721819537d8011ef7131665b106e71a5a78
Martin Herkt <lachs0r@srsfckn.biz>
2017-12-24 19:42:53 +0100

    opus: update to 1.3-beta

--- packages/opus.cmake
+++ packages/opus.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(opus
     DEPENDS gcc
-    URL "https://archive.mozilla.org/pub/opus/opus-1.2.1.tar.gz"
-    URL_HASH SHA256=cfafd339ccd9c5ef8d6ab15d7e1a412c054bf4cb4ecbbbcc78c12ef2def70732
+    URL "https://archive.mozilla.org/pub/opus/opus-1.3-beta.tar.gz"
+    URL_HASH SHA256=7f56e058c9549d03ae35511ad9e16ef6d1eb257836830d54abff0f495f17e187
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
