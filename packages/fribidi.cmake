41f1e0ee4301fc2f7034f89ef9b87186fc66b117
Martin Herkt <lachs0r@srsfckn.biz>
2018-03-06 22:20:16 +0100

    fribidi: switch to meson

--- packages/fribidi.cmake
+++ packages/fribidi.cmake
@@ -1,27 +1,26 @@
-if(NOT WIN32)
-    find_program(WINE wine)
-    set(TARGET_EXEC "TARGET_EXEC=${WINE}")
+find_program(MESON meson)
+find_program(NINJA ninja)
+
+if((NOT MESON) OR (NOT NINJA))
+    message(FATAL_ERROR "Meson and Ninja are required to build fribidi")
 endif()
 
 ExternalProject_Add(fribidi
     DEPENDS gcc
     GIT_REPOSITORY "https://github.com/fribidi/fribidi.git"
     UPDATE_COMMAND ""
-    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/fribidi-*.patch
-    # certain env variables with spaces confuse this broken build system
-    CONFIGURE_COMMAND ${EXEC} PKG_CONFIG=pkg-config <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E remove_directory <BINARY_DIR>
+        COMMAND ${CMAKE_COMMAND} -E make_directory <BINARY_DIR>
+        COMMAND ${EXEC} cd <BINARY_DIR> &&
+        ${MESON}
         --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-        --disable-deprecated
-        --without-glib
-        --enable-charsets
-        --enable-malloc
-    BUILD_COMMAND ${MAKE} -j1 ${TARGET_EXEC} # breaks with parallel make
-    INSTALL_COMMAND ${MAKE} -j1 install
-    BUILD_IN_SOURCE 1
+        --cross-file=${MESON_CROSS}
+        --default-library=static
+        -Ddocs=false
+        <SOURCE_DIR>
+    BUILD_COMMAND ${NINJA}
+    INSTALL_COMMAND ${NINJA} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(fribidi)
-autoreconf(fribidi)
