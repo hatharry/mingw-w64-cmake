bf2d134246a7e352a2c760a1b69edfbc6b9f3ced
Martin Herkt <lachs0r@srsfckn.biz>
2018-03-06 21:34:27 +0100

    libzip: update to 1.4.0, switch to cmake, fix patch

--- packages/libzip.cmake
+++ packages/libzip.cmake
@@ -1,15 +1,11 @@
 ExternalProject_Add(libzip
     DEPENDS zlib bzip2
-    URL "https://libzip.org/download/libzip-1.3.0.tar.xz"
-    URL_HASH SHA256=aa936efe34911be7acac2ab07fb5c8efa53ed9bb4d44ad1fe8bff19630e0d373
+    URL "https://libzip.org/download/libzip-1.4.0.tar.xz"
+    URL_HASH SHA256=e508aba025f5f94b267d5120fc33761bcd98440ebe49dbfe2ed3df3afeacc7b1
     PATCH_COMMAND patch -p1 -i ${CMAKE_CURRENT_SOURCE_DIR}/libzip-fixes.patch
-    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
-        --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX} -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} -DCMAKE_BUILD_TYPE=RelWithDebInfo
+        -DBUILD_SHARED_LIBS=OFF
+    BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
+    INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
-
-autoreconf(libzip)
