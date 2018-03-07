220511b92495031d4b31994d975725ea80595be6
Martin Herkt <lachs0r@srsfckn.biz>
2018-03-07 08:08:06 +0100

    wxWidgets: update to 3.1.1

--- packages/wxWidgets.cmake
+++ packages/wxWidgets.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(wxWidgets
     DEPENDS gcc expat libiconv libjpeg libpng
-    URL "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2"
-    URL_HASH SHA256=e082460fb6bf14b7dd6e8ac142598d1d3d0b08a7b5ba402fdbf8711da7e66da8
+    URL "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.1/wxWidgets-3.1.1.tar.bz2"
+    URL_HASH SHA256=c925dfe17e8f8b09eb7ea9bfdcfcc13696a3e14e92750effd839f5e10726159e
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
@@ -20,20 +20,10 @@ ExternalProject_Add(wxWidgets
         --with-expat=sys
         --without-sdl
         --without-gtk
-        --without-motif
-        --without-mac
-        --without-macosx-sdk
-        --without-cocoa
-        --without-wine
-        --without-pm
-        --without-microwin
         --without-libxpm
         --without-libmspack
-        --without-gnomeprint
         --without-gnomevfs
-        --without-hildon
         --without-dmalloc
-        --without-odbc
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
         COMMAND ${CMAKE_COMMAND} -E create_symlink ${MINGW_INSTALL_PREFIX}/bin/wx-config ${CMAKE_INSTALL_PREFIX}/bin/wx-config
