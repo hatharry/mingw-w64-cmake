6bb028cb46198ff5ed94e297826e2ee5a94bc745
Martin Herkt <lachs0r@srsfckn.biz>
2018-03-16 23:15:20 +0100

    vorbis: update to 1.3.6

--- packages/vorbis.cmake
+++ packages/vorbis.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(vorbis
     DEPENDS ogg
-    URL "https://ftp.osuosl.org/pub/xiph/releases/vorbis/libvorbis-1.3.5.tar.xz"
-    URL_HASH SHA256=54f94a9527ff0a88477be0a71c0bab09a4c3febe0ed878b24824906cd4b0e1d1
+    URL "https://ftp.osuosl.org/pub/xiph/releases/vorbis/libvorbis-1.3.6.tar.xz"
+    URL_HASH SHA256=af00bb5a784e7c9e69f56823de4637c350643deedaf333d0fa86ecdba6fcb415
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
